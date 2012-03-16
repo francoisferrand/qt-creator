5538608d9ca4c75aff058c97e0d23810af654c76
Francois Ferrand <thetypz@gmail.com>
2012-03-16 16:11:41 +0100

    Allow macro expansion in FastPreprocessor.
    This is required to properly highlight code were the enclosing method
    definition is generated.
    
    Change-Id: I2d28270563eebb495b82fac8ec23346c98e859f9

--- src/plugins/cppeditor/cppeditor.cpp
+++ src/plugins/cppeditor/cppeditor.cpp
@@ -790,6 +790,7 @@ void CPPEditorWidget::markSymbolsNow()
     const QList<int> result = m_references.result();
 
     QList<QTextEdit::ExtraSelection> selections;
+    QSet<int> highlightedSymbolPositions;
 
     foreach (int index, result) {
         unsigned line, column;
@@ -804,11 +805,15 @@ void CPPEditorWidget::markSymbolsNow()
         cursor.setPosition(cursor.position() + column);
         cursor.setPosition(cursor.position() + len, QTextCursor::KeepAnchor);
 
+        //Do not highlight twice the same position which may happen for macro parameters
+        if (highlightedSymbolPositions.contains(cursor.anchor()))
+            continue;
+        highlightedSymbolPositions.insert(cursor.anchor());
+
         QTextEdit::ExtraSelection sel;
         sel.format = m_occurrencesFormat;
         sel.cursor = cursor;
         selections.append(sel);
-
     }
 
     setExtraSelections(CodeSemanticsSelection, selections);
